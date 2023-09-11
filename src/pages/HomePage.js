21c14ff769925588f09f13ea9f3c6074ef824d26
Peter Huynh <peterhyh@yahoo.com>
2023-09-11 17:42:31 -0500

    added 3 photos to assets

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,19 @@
+import { Parallax } from "react-scroll-parallax";
+import { ParallaxProvider } from "react-scroll-parallax";
+import BO from '../app/assets/img/Bo.jpg';
+
 const HomePage = () => {
+
+
     return (
-        <div>
-            <p>Photography Portfolio</p>
+        <ParallaxProvider>
             <div>
-                <p>Images</p>
-                <img />
+                <p>Photography Portfolio</p>
+                <Parallax translateY={['100px', '-100px']} >
+                    <img height='500px' src={BO} alt='Bo holding an umbrella' />
+                </Parallax>
             </div>
-        </div>
+        </ParallaxProvider>
     )
 };
 
